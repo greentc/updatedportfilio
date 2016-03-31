021844ebb1a60d50cfb2ceba4177362c002576bf
greentc <tcgreen@umich.edu>
2016-03-31 11:20:45 -0400

    NAV BAR CHANGES
    I was lazy so I just dropped my entire static folder in with the upload instead of just fetching the lone css file. I FINALLY USED PYTHON AND CLASSES AND MY BASE TEMPLATE TO CHANGE THE NAV. YES.

--- main.py
+++ main.py
@@ -27,51 +27,53 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-    	try:
-    		template = JINJA_ENVIRONMENT.get_template('templates%s' % self.request.path)
-    		logging.info("Determing which page to render")
+        try:
+            template = JINJA_ENVIRONMENT.get_template('templates%s' % self.request.path)
+            logging.info("Determing which page to render")
+            if self.request.path == '/projects.html':
+                self.response.write(template.render({'Home' : 'Home', 'Projects' : 'PROJECTS', 'Contact' : 'Contact', 'Journey': 'Journey' ,'activep' : 'factive'} ))
+                logging.info("Projects template rendered")
 
-    		if self.request.path == '/projects.html':
-    			self.response.write(template.render())
-    			logging.info("Projects template rendered")
-    		if self.request.path =='/contact.html':
-    			self.response.write(template.render())
-    			logging.info("Contact template rendered)")
-    		if self.request.path == '/journey.html':
-    			self.response.write(template.render())
-    			logging.info("Journey template rendered")
-    		if self.request.path == '/home.html':
-    			self.response.write(template.render())
-    			logging.info("Home template rendered")
-    	except:
-    		template = JINJA_ENVIRONMENT.get_template('templates/home.html')
-        	self.response.write(template.render())
-        	logging.info("No path requested, page rendered manually to Home template")
+            if self.request.path =='/contact.html':
+                self.response.write(template.render({'Home' : 'Home', 'Projects' : 'Projects', 'Contact' : 'CONTACT', 'Journey': 'Journey' ,'activec' : 'active'} ))
+                logging.info("Contact template rendered)")
 
-    # def post(self):
-    #     try:
-    #         reason = self.request.get("reason")
-    #         logging.info(reason)
-    #         email = self.request.get("email")
-    #         message = self.request.get("message")
-    #         outfile = open("contactdata.csv" , "w")
-    #         outfile.write( "%s, %s, %s\n" % (email, reason, message))
-    #         outfile.close()
-    #         template = JINJA_ENVIRONMENT.get_template('templates/contact.html')
-    #     except:
-    #         logging.info("csv file could not be created")
-    #         template = JINJA_ENVIRONMENT.get_template('templates/home.html')
+            if self.request.path == '/journey.html':
+                self.response.write(template.render({'Home' : 'Home', 'Projects' : 'Projects', 'Contact' : 'Contact', 'Journey': 'JOURNEY', 'activej' : 'active'} ))
+                logging.info("Journey template rendered")
+
+            if self.request.path == '/home.html':
+                self.response.write(template.render({'Home' : 'HOME', 'Projects' : 'Projects', 'Contact' : 'Contact', 'Journey': 'Journey', 'activeh' : 'active' } ))
+                logging.info("Home template rendered")
+        except:
+            template = JINJA_ENVIRONMENT.get_template('templates/home.html')
+            self.response.write(template.render({'Home': 'HOME', 'Projects' : 'Projects', 'Contact' : 'Contact', 'Journey': 'Journey'} ))
+            logging.info("No path requested, page rendered manually to Home template")
+
+    def post(self):
+        try:
+            reason = self.request.get("reason")
+            logging.info(reason)
+            email = self.request.get("email")
+            message = self.request.get("message")
+            outfile = open("contactdata.csv" , "w")
+            outfile.write( "%s, %s, %s\n" % (email, reason, message))
+            outfile.close()
+            template = JINJA_ENVIRONMENT.get_template('templates/contact.html')
+        except:
+            logging.info("csv file could not be created")
+            template = JINJA_ENVIRONMENT.get_template('templates/home.html')
     
-            	
+                
 
-    						
-    			
+                            
+                
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-	('/home.html', MainHandler),
-	('/journey.html', MainHandler),
-	('/contact.html', MainHandler),
-	('/projects.html', MainHandler)
+    ('/home.html', MainHandler),
+    ('/journey.html', MainHandler),
+    ('/contact.html', MainHandler),
+    ('/projects.html', MainHandler)
 ], debug=True)
